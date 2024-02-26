701557d131d531ad7ad2d5c312d9bdd6edebe91f
Kaktys36 <nikolaeff.alexandr2016@yandex.ru>
2024-02-26 19:14:55 +0300

    info

--- info_script.py
+++ info_script.py
@@ -1,25 +1,26 @@
 import streamlit as st
 
 def info_func(info, session_status):    
-    info_btn = st.button('Показать/Скрыть информацию о проекте')
+    if session_status.show_start_page:
+        info_btn = st.button('Показать/Скрыть информацию о проекте')
 
-    if info_btn:
-        if 'show_info' not in session_status or not session_status.show_info:
-            st.session_state.show_info = True
-        else:
-            st.session_state.show_info = False
+        if info_btn:
+            if 'show_info' not in session_status or not session_status.show_info:
+                st.session_state.show_info = True
+            else:
+                st.session_state.show_info = False
 
-    if st.session_state.show_info:
-        infos = ['Манифест', 'Поддержать проект', 'Контакты', 'Последние обновления', 'Выражаю благодарность']
-        selected_info = st.selectbox('Информация о проекте.', infos)
+        if st.session_state.show_info:
+            infos = ['Манифест', 'Поддержать проект', 'Контакты', 'Последние обновления', 'Выражаю благодарность']
+            selected_info = st.selectbox('Информация о проекте.', infos)
 
-    if selected_info == 'Манифест':
-        st.text(info[0])
-    elif selected_info == 'Поддержать проект':
-        st.text(info[1])
-    elif selected_info == 'Контакты':
-        st.text(info[2])
-    elif selected_info == 'Последние обновления':
-        st.text(info[3])
-    elif selected_info == 'Выражаю благодарность':
-        st.text(info[4])
+        if selected_info == 'Манифест':
+            st.text(info[0])
+        elif selected_info == 'Поддержать проект':
+            st.text(info[1])
+        elif selected_info == 'Контакты':
+            st.text(info[2])
+        elif selected_info == 'Последние обновления':
+            st.text(info[3])
+        elif selected_info == 'Выражаю благодарность':
+            st.text(info[4])
