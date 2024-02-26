489aa82eb023aa5bcc73381b8f554e73f37bcd49
Kaktys36 <nikolaeff.alexandr2016@yandex.ru>
2024-02-26 16:47:16 +0300

    infos

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ import streamlit as st
 from open_ai_gpt import ChatBot
 from YOLO_face_detection import FaceDetector
 
-
-st.title('Neuro_tool_v_1_1')
-st.subheader(
-    'Это проект "Нейросетевого мультитула". Суть в том, что здесь \
+show_start_page = True
+if show_start_page:
+    st.title('Neuro_tool_v.1.1')
+    st.subheader(
+       'Это проект "Нейросетевого мультитула". Суть в том, что здесь \
         собраны воедино несколько нейросетевых инструментов. \
-            Для выбора модели используй выпадающий список.')
+        Для выбора модели используй выпадающий список.')
 
 models = ['GPT 3.5_turbo', 'YOLO_face_detecton']
 selected_model = st.selectbox('Выберите модель: ', models)
@@ -18,12 +19,12 @@ if selected_model == 'GPT 3.5_turbo':
 
     st.button("Вернуться к выбору модели")
     if st.button:
-        st.experimental_rerun()
+        st.rerun()
 
 elif selected_model == 'YOLO_face_detecton':
     face_detector = FaceDetector()
     face_detector.run()
-    
+
     st.button("Вернуться к выбору модели")
     if st.button:
-        st.experimental_rerun()
+        st.rerun()
