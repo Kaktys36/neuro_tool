752f8091dbbac549742628cd4de2171a77a97e10
Kaktys36 <nikolaeff.alexandr2016@yandex.ru>
2024-02-26 20:14:17 +0300

    info

--- main.py
+++ main.py
@@ -10,15 +10,14 @@ if "show_start_page" not in session_status:
     session_status.show_info = False
 
 if session_status.show_start_page:
-    st.title('Neuro_tool_v.1.1')
-    st.subheader(
-       'Это проект "Нейросетевого мультитула". Суть в том, что здесь собраны воедино несколько нейросетевых инструментов. Всё доступно каждому и абсолютно бесплатно (подробнее в информация_о_проекте>манифест).')
 
-    #session_status.show_start_page = False
 models = ['Выбрать модель', 'GPT 3.5_turbo', 'YOLO8_face_detecton']
 selected_model = st.selectbox('Выберите модель: ', models)
 
 if session_status.show_start_page:
+    st.title('Neuro_tool_v.1.1')
+    st.subheader(
+       'Это проект "Нейросетевого мультитула". Суть в том, что здесь собраны воедино несколько нейросетевых инструментов. Всё доступно каждому и абсолютно бесплатно (подробнее в информация_о_проекте>манифест).')
     
     if selected_model == 'GPT 3.5_turbo':
         chat_bot = ChatBot()
