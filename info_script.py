05fd481b47d4a1a93be54c2399b2ef0123e2574d
Kaktys36 <nikolaeff.alexandr2016@yandex.ru>
2024-02-26 19:26:33 +0300

    info

--- info_script.py
+++ info_script.py
@@ -12,17 +12,18 @@ def info_func(info, session_status):
 
         if st.session_state.show_info:
             infos = ['Манифест', 'Поддержать проект', 'Контакты', 'Последние обновления', 'Выражаю благодарность']
-            selected_info = st.selectbox('Информация о проекте.', infos)
+            selected_info = st.selectbox('Информация о проекте.', infos, index=0)
 
-        if selected_info == 'Манифест':
-            st.text(info[0])
-        elif selected_info == 'Поддержать проект':
-            st.text(info[1])
-        elif selected_info == 'Контакты':
-            st.text(info[2])
-        elif selected_info == 'Последние обновления':
-            st.text(info[3])
-        elif selected_info == 'Выражаю благодарность':
-            st.text(info[4])
-    else:
-        pass
+        if 'selected_info' in locals():  # Проверяем, определена ли переменная selected_info
+            if selected_info == 'Манифест':
+                st.text(info[0])
+            elif selected_info == 'Поддержать проект':
+                st.text(info[1])
+            elif selected_info == 'Контакты':
+                st.text(info[2])
+            elif selected_info == 'Последние обновления':
+                st.text(info[3])
+            elif selected_info == 'Выражаю благодарность':
+                st.text(info[4])
+        else:
+            pass
