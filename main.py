7743998f4041a36c829343ae5da7be573dc33f9a
Kaktys36 <nikolaeff.alexandr2016@yandex.ru>
2024-02-26 20:29:29 +0300

    info

--- main.py
+++ main.py
@@ -4,15 +4,13 @@ from YOLO_face_detection import FaceDetector
 from info_script import info_func
 from info_data import info
 
-session_status = st.session_state
-if "show_start_page" not in session_status:
-    session_status.show_start_page = True
-    session_status.show_info = False
+show_start_page = True
+show_info = True
 
 models = ['Выбрать модель', 'GPT 3.5_turbo', 'YOLO8_face_detecton']
 selected_model = st.selectbox('Выберите модель: ', models)
 
-if session_status.show_start_page:
+if show_start_page:
     st.title('Neuro_tool_v.1.1')
     st.subheader(
        'Это проект "Нейросетевого мультитула". Суть в том, что здесь собраны воедино несколько нейросетевых инструментов. Всё доступно каждому и абсолютно бесплатно (подробнее в информация_о_проекте>манифест).')
@@ -20,14 +18,14 @@ if session_status.show_start_page:
     if selected_model == 'GPT 3.5_turbo':
         chat_bot = ChatBot()
         chat_bot.run()
-        session_status.show_start_page = False
+        show_start_page = False
 
     elif selected_model == 'YOLO8_face_detecton':
         face_detector = FaceDetector()
         face_detector.run()
-        session_status.show_start_page = False
+        show_start_page = False
 
     else:
-        session_status.show_start_page = True
+        show_start_page = True
 
-info_func(info, session_status)
+#info_func(info, session_status)
