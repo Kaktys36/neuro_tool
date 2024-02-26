38835301cce4c84b36cb9624dff7d26d8b3f9880
Kaktys36 <nikolaeff.alexandr2016@yandex.ru>
2024-02-26 19:28:25 +0300

    info

--- main.py
+++ main.py
@@ -23,10 +23,12 @@ if not session_status.show_start_page:
     selected_model = st.selectbox('Выберите модель: ', models)
 
     if selected_model == 'GPT 3.5_turbo':
+        session_status.show_start_page = False
         chat_bot = ChatBot()
         chat_bot.run()
 
     elif selected_model == 'YOLO_face_detecton':
+        session_status.show_start_page = False
         face_detector = FaceDetector()
         face_detector.run()
 
