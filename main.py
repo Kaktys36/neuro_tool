c3f29f53bd9069ec4555aa4a57226aefe9a84a6a
Kaktys36 <nikolaeff.alexandr2016@yandex.ru>
2024-02-26 18:19:58 +0300

    infos

--- main.py
+++ main.py
@@ -26,17 +26,16 @@ elif selected_model == 'YOLO_face_detecton':
 
 st.session_state.show_start_page = False
 
-else:
-    infos = ['Манифест', 'Поддержать проект', 'Контакты', 'Последние обновления', 'Выражаю благодарность']
-    selected_info = st.selectbox('Информация о проекте.', infos)
+infos = ['Манифест', 'Поддержать проект', 'Контакты', 'Последние обновления', 'Выражаю благодарность']
+selected_info = st.selectbox('Информация о проекте.', infos)
 
-    if selected_info == 'Манифест':
-        st.text(info[0])
-    elif selected_info == 'Поддержать проект':
-        st.text(info[1])
-    elif selected_info == 'Контакты':
-        st.text(info[2])
-    elif selected_info == 'Последние обновления':
-        st.text(info[3])
-    elif selected_info == 'Выражаю благодарность':
-        st.text(info[4])
+if selected_info == 'Манифест':
+    st.text(info[0])
+elif selected_info == 'Поддержать проект':
+    st.text(info[1])
+elif selected_info == 'Контакты':
+    st.text(info[2])
+elif selected_info == 'Последние обновления':
+    st.text(info[3])
+elif selected_info == 'Выражаю благодарность':
+    st.text(info[4])
