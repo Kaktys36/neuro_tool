0d17955f5c7eda7b49350a50a0f98d764bccb8ba
Kaktys36 <nikolaeff.alexandr2016@yandex.ru>
2024-02-26 21:04:06 +0300

    111

--- info_script.py
+++ info_script.py
@@ -1,10 +1,12 @@
 import streamlit as st
-def click_button():
-    st.session_state.clicked = True
 
 def info_func(info, show_info, show_start_page):    
+    def click_button():
+        st.session_state.clicked = True
+
     if 'clicked' not in st.session_state:
     st.session_state.clicked = False
+    
     if show_start_page:
         info_btn = st.button('Показать/Скрыть информацию о проекте', on_click=click_button)
         print(info_btn)
