eef8031258abea569060c9b2b856e4dda7b17038
Kaktys36 <nikolaeff.alexandr2016@yandex.ru>
2024-02-26 18:17:22 +0300

    infos

--- main.py
+++ main.py
@@ -3,44 +3,40 @@ from open_ai_gpt import ChatBot
 from YOLO_face_detection import FaceDetector
 from info import info
 
-show_start_page = True
-if show_start_page:
+# Инициализация сессионного состояния
+if "show_start_page" not in st.session_state:
+    st.session_state.show_start_page = True
+
+if st.session_state.show_start_page:
     st.title('Neuro_tool_v.1.1')
     st.subheader(
        'Это проект "Нейросетевого мультитула". Суть в том, что здесь \
         собраны воедино несколько нейросетевых инструментов. Всё доступно каждому и абсолютно бесплатно (подробнее в манифесте).')
 
-models = ['GPT 3.5_turbo', 'YOLO_face_detecton']
-selected_model = st.selectbox('Выберите модель: ', models)
+    models = ['GPT 3.5_turbo', 'YOLO_face_detecton']
+    selected_model = st.selectbox('Выберите модель: ', models)
 
-if selected_model == 'GPT 3.5_turbo':
-    chat_bot = ChatBot()
-    chat_bot.run()
+    if selected_model == 'GPT 3.5_turbo':
+        chat_bot = ChatBot()
+        chat_bot.run()
 
-    re_btn =  st.button("Вернуться к начальной странице")
-    if re_btn:
-        show_start_page = False
-        st.rerun()
+    elif selected_model == 'YOLO_face_detecton':
+        face_detector = FaceDetector()
+        face_detector.run()
 
-elif selected_model == 'YOLO_face_detecton':
-    face_detector = FaceDetector()
-    face_detector.run()
+    st.session_state.show_start_page = False
 
-    re_btn =  st.button("Вернуться к начальной странице")
-    if re_btn:
-        show_start_page = False
-        st.rerun()
+else:
+    infos = ['Манифест', 'Поддержать проект', 'Контакты', 'Последние обновления', 'Выражаю благодарность']
+    selected_info = st.selectbox('Информация о проекте.', infos)
 
-infos = ['Манифест', 'Поддержать проект', 'Контакты', 'Последние обновления', 'Выражаю благодарность']
-selected_info = st.selectbox('Информация о проекте.', infos)
-
-if selected_info == 'Манифест':
-    st.text(info[0])
-elif selected_info == 'Поддержать проект':
-    st.text(info[1])
-elif selected_info == 'Контакты':
-    st.text(info[2])
-elif selected_info == 'Последние обновления':
-    st.text(info[3])
-elif selected_info == 'Выражаю благодарность':
-    st.text(info[4])
+    if selected_info == 'Манифест':
+        st.text(info[0])
+    elif selected_info == 'Поддержать проект':
+        st.text(info[1])
+    elif selected_info == 'Контакты':
+        st.text(info[2])
+    elif selected_info == 'Последние обновления':
+        st.text(info[3])
+    elif selected_info == 'Выражаю благодарность':
+        st.text(info[4])
