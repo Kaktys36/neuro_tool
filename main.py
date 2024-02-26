191cd6addb3934fb2ee32caec5928625bf6e93a1
Kaktys36 <nikolaeff.alexandr2016@yandex.ru>
2024-02-26 19:01:53 +0300

    info

--- main.py
+++ main.py
@@ -6,11 +6,11 @@ from info_data import info
 
 # Инициализация сессионного состояния
 session_status = st.session_state
-if "show_start_page" not in st.session_status:
-    st.session_state.show_start_page = True
-    st.session_state.show_info = False
+if "show_start_page" not in session_status:
+    session_status.show_start_page = True
+    session_status.show_info = False
 
-if st.session_state.show_start_page:
+if session_status.show_start_page:
     st.title('Neuro_tool_v.1.1')
     st.subheader(
        '''Это проект "Нейросетевого мультитула". Суть в том, что здесь \
@@ -33,5 +33,5 @@ elif selected_model == 'YOLO_face_detecton':
     face_detector = FaceDetector()
     face_detector.run()
 
-st.session_state.show_start_page = False
+session_status.show_start_page = False
 
