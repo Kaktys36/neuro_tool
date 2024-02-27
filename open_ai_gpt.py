2f85f66dead175263aa29b5f1a5af3a775e6bbaf
Kaktys36 <nikolaeff.alexandr2016@yandex.ru>
2024-02-27 15:19:14 +0300

    chat_win

--- open_ai_gpt.py
+++ open_ai_gpt.py
@@ -15,6 +15,14 @@ class ChatBot:
         if "messages" not in st.session_state:
             st.session_state.messages = []
 
+        st.markdown("""
+        <style>
+        div[data-baseweb="input"] {
+            margin-top: -100px;  # Меняйте значение, чтобы придать нужное смещение
+        }
+        </style>
+        """, unsafe_allow_html=True)
+        
         for message in st.session_state.messages:
             with st.chat_message(message["role"]):
                 st.markdown(message["content"])
