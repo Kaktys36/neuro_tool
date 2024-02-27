56c444c61f75fc176658fdb726c0f1a63fd848b8
Kaktys36 <nikolaeff.alexandr2016@yandex.ru>
2024-02-27 20:51:23 +0300

    111

--- info_script.py
+++ info_script.py
@@ -1,29 +1,29 @@
 import streamlit as st
 
 def info_func(info, show_info, show_start_page):    
-    if show_start_page:
-        info_toggle = st.checkbox('Показать/Скрыть информацию о проекте')
+    #if show_start_page:
+    info_toggle = st.checkbox('Показать/Скрыть информацию о проекте')
 
-        if info_toggle:
-            show_info = True
-        else:
-            show_info = False
+    if info_toggle:
+        show_info = True
+    else:
+        show_info = False
 
-        if show_info:
-            infos = ['Манифест', 'Поддержать проект', 'Контакты', 'Последние обновления', 'Выражаю благодарность']
-            selected_info = st.selectbox('Информация о проекте.', infos, index=0)
+    if show_info:
+        infos = ['Манифест', 'Поддержать проект', 'Контакты', 'Последние обновления', 'Выражаю благодарность']
+        selected_info = st.selectbox('Информация о проекте.', infos, index=0)
 
-        if 'selected_info' in locals():  # Проверяем, определена ли переменная selected_info
-            if selected_info == 'Манифест':
-                st.text(info[0])
-            elif selected_info == 'Поддержать проект':
-                st.text(info[1])
-            elif selected_info == 'Контакты':
-                st.text(info[2])
-            elif selected_info == 'Последние обновления':
-                st.text(info[3])
-            elif selected_info == 'Выражаю благодарность':
-                st.text(info[4])
-        else:
-            pass
+    if 'selected_info' in locals():  # Проверяем, определена ли переменная selected_info
+        if selected_info == 'Манифест':
+            st.text(info[0])
+        elif selected_info == 'Поддержать проект':
+            st.text(info[1])
+        elif selected_info == 'Контакты':
+            st.text(info[2])
+        elif selected_info == 'Последние обновления':
+            st.text(info[3])
+        elif selected_info == 'Выражаю благодарность':
+            st.text(info[4])
+    else:
+        pass
 
