08d5c1b4ed926964d7ed1a70f462d05d0b47d4c3
Kaktys36 <nikolaeff.alexandr2016@yandex.ru>
2024-03-03 15:08:06 +0300

    11

--- open_ai_gpt.py
+++ open_ai_gpt.py
@@ -59,7 +59,7 @@ class ChatBot:
                             ''')
             
             if scenario := st.chat_input(rand_scenario):
-                st.write scenario
+                st.write(scenario)
             #user_scenario = st.chat_input(str(rand_scenario))
             #scenario = user_scenario
             #st.session_state.messages.append({'role': 'system', 'content': scenario})
