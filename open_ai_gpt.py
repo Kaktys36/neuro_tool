8d57561ee78f6f3209af4853858d93f084d0fb45
Kaktys36 <nikolaeff.alexandr2016@yandex.ru>
2024-03-03 16:13:11 +0300

    11

--- open_ai_gpt.py
+++ open_ai_gpt.py
@@ -58,10 +58,10 @@ class ChatBot:
                             перезагрузить страницу.
                             ''')
             
-            scenario = st.text_input(rand_scenario)
+            #scenario = st.chat_input(rand_scenario)
                 #print('12312')
-            #user_scenario = st.chat_input(str(rand_scenario))
-            #scenario = user_scenario
+            user_scenario = st.chat_input(str(rand_scenario))
+            scenario = user_scenario
             st.session_state.messages.append({'role': 'system', 'content': scenario})
         else:
             pass
