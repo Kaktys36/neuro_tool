ec4895a8b341af3d9c277f14c81c4d0d8b3fd74c
Kaktys36 <nikolaeff.alexandr2016@yandex.ru>
2024-03-03 14:42:26 +0300

    111

--- open_ai_gpt.py
+++ open_ai_gpt.py
@@ -58,11 +58,9 @@ class ChatBot:
                             бот забыл роль, то нужно
                             перезагрузить страницу.
                             ''')
-            if scenario := st.chat_input(f'{rand_scenario}'):
-                print(scenario)
-                print(111)
-                st.session_state.messages.append({'role': 'system', 'content': scenario})
-                scenario = ''
+            scenario := st.chat_input(f'{rand_scenario}'):
+            st.session_state.messages.append({'role': 'system', 'content': scenario})
+            scenario = ''
 
 
         for message in st.session_state.messages:
