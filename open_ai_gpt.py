720cfecccab53ca6ade24b9b02266a6e7f99226a
Kaktys36 <nikolaeff.alexandr2016@yandex.ru>
2024-03-08 15:19:30 +0300

    123

--- open_ai_gpt.py
+++ open_ai_gpt.py
@@ -87,6 +87,4 @@ class ChatBot:
                     message_placeholder.markdown(full_response + "▌")
                 message_placeholder.markdown(full_response)
             st.session_state.messages.append({"role": "assistant", "content": full_response})
-            
-            title = st.text_input('Movie title', 'Life of Brian')
-            st.write('The current movie title is', title)
+        
