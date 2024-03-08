4ab793abef98b09bffe79361060b4df7585401b6
Kaktys36 <nikolaeff.alexandr2016@yandex.ru>
2024-03-08 16:10:44 +0300

    123

--- open_ai_gpt.py
+++ open_ai_gpt.py
@@ -60,7 +60,7 @@ class ChatBot:
                             ''')
                 
             rand_scenario = random.choice(scenario_examples)
-            self.scenario = st.text_input(label=rand_scenario, value='Введите сценарий в это поле.')
+            self.scenario = st.text_area(label=rand_scenario, value='Введите сценарий в это поле.')
             st.info(self.scenario)
             if st.button("Сохранить"):
                 if self.scenario is not 'Введите сценарий в это поле.':
