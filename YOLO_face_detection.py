c362e50f01c2ced31eff7ed7caae60cc4626a833
Denis Khlamov <133785829+XDM94@users.noreply.github.com>
2024-03-08 14:13:46 +0300

    Update YOLO_face_detection.py
    Change url image

--- YOLO_face_detection.py
+++ YOLO_face_detection.py
@@ -16,15 +16,15 @@ class FaceDetector:
         uploaded_file = st.file_uploader('Выберите изображение (jpg, jpeg, png)', type=['jpg', 'jpeg', 'png'])
         url = st.text_input("Введите ссылку на изображения")
 
-        if url is not None:
-            image = Image.open(url)
+     
+        if uploaded_file is not None:
+            image = Image.open(uploaded_file)
             output = self.model(image)
             results = Detections.from_ultralytics(output[0])
             st.write(f'Модель обнаружила на фотографии {len(results)} лиц людей.')
-        
 
-        if uploaded_file is not None:
-            image = Image.open(uploaded_file)
+           elif url is not None:
+            image = Image.open(url)
             output = self.model(image)
             results = Detections.from_ultralytics(output[0])
             st.write(f'Модель обнаружила на фотографии {len(results)} лиц людей.')
