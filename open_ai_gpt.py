8fa6386d2241c29af13df1afa3e3fc287bc16483
Kaktys36 <nikolaeff.alexandr2016@yandex.ru>
2024-03-08 15:20:53 +0300

    123

--- open_ai_gpt.py
+++ open_ai_gpt.py
@@ -28,7 +28,7 @@ class ChatBot:
             'Пример. Запомни: мы говорили про насекомых, говори только про них. Своди все диалоги к ним.'
             'Пример. Запомни: говори только о котиках, своди все разговоры к ним.'
                              ]
-        if st.sidebar.checkbox("Добавить сценарий"):
+        with st.sidebar.checkbox("Добавить сценарий"):
             rand_scenario = random.choice(scenario_examples)
             st.sidebar.markdown('''
                             Данная функция позволяет 
