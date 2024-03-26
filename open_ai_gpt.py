5eef084f9eee2beed4c1c900a765073ddc4b0c1f
Kaktys36 <nikolaeff.alexandr2016@yandex.ru>
2024-03-26 14:53:44 +0300

    123

--- open_ai_gpt.py
+++ open_ai_gpt.py
@@ -73,14 +73,7 @@ class ChatBot:
             self.scenario = st.chat_input('Поле для ввода сценария.')
             if self.scenario is not None:
                 st.session_state.messages.append({'role': 'system', 'content': self.scenario})
-            #self.scenario = st.text_area(
-            #    label=rand_scenario, value='Введите сценарий в это поле.'
-            #)
-            print(self.scenario)
-            #if st.button('Сохранить'):
-            #if self.scenario != 'Введите сценарий в это поле.':
-            #st.session_state.messages.append({'role': 'system', 'content': self.scenario})
-
+                
         for message in st.session_state.messages:
             with st.chat_message(message['role']):
                 st.markdown(message['content'])
