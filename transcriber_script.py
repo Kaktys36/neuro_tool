ac15e77ff7727e3bc52c40f2519ef1085a1123e0
Kaktys36 <nikolaeff.alexandr2016@yandex.ru>
2024-05-04 13:42:16 +0300

    123

--- transcriber_script.py
+++ transcriber_script.py
@@ -9,8 +9,12 @@ class Transcriber:
         
     def run(self):
         st.title("Whisper")
-        if st.sidebar.form("input_form"):
-            input_files = st.file_uploader("Files", type=["mp4", "m4a", "mp3", "wav"], accept_multiple_files=True)
+        st.subheader(
+            """
+                    Я могу писать транскрибировать аудио и видеофайлы (получать текст).
+                     """
+        )
+        input_files = st.sidebar.file_uploader("Files", type=["mp4", "m4a", "mp3", "wav"], accept_multiple_files=True)
 
-            whisper_model = st.selectbox("Whisper model", options=[
+        whisper_model = st.sidebar.selectbox("Whisper model", options=[
             "tiny", "base", "small", "medium", "large"], index=4)
