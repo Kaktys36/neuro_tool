25eba0f6849a164d9a5d867807ca4b5d06b4901b
Kaktys36 <nikolaeff.alexandr2016@yandex.ru>
2024-05-04 15:35:49 +0300

    123

--- transcriber_script.py
+++ transcriber_script.py
@@ -21,7 +21,7 @@ class Transcriber:
         self.whisper_model = st.sidebar.selectbox("Whisper model", options=[
             "tiny", "base", "small", "medium", "large", "large-v2", "large-v3"], index=4)
         
-    def load_whisper_model(whisper_model: str) -> whisper.Whisper:
+    def load_whisper_model(whisper_model: str):
         try:
             global model
             model = whisper.load_model(whisper_model)
