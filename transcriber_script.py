c6ddaf9d3f56e6162c22a3e06d2fb9f0f7b5dc3d
Kaktys36 <nikolaeff.alexandr2016@yandex.ru>
2024-05-04 16:38:05 +0300

    123

--- transcriber_script.py
+++ transcriber_script.py
@@ -8,8 +8,9 @@ class Transcriber:
     def __init__(self):
         self.test = 'asdas',
         self.whisper_model = "base"
+        @st.cache_resource(show_spinner=False)
         
-    def load_whisper_model(self, modelName: str): #-> whisper.Whisper:
+    def load_whisper_model(self, modelName: str) -> whisper.Whisper:
         try:
             global model
             model = whisper.load_model(modelName)
