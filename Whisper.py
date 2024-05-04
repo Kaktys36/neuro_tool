483d9b47342b0da93c5ffbd5113b5910681a8845
Kaktys36 <nikolaeff.alexandr2016@yandex.ru>
2024-05-04 13:26:07 +0300

    123

--- Whisper.py
+++ Whisper.py
@@ -2,17 +2,14 @@ import whisper
 import streamlit as st
 
 class Transcriber:
-    def __init__(
-        self,
-        whisper_model: str,
-        translation: bool):
+    def __init__(self, whisper_model: str, translation: bool):
         self.translation = False
         self.whisper_model = 'base'
         
     def run():
-        with st.sidebar.form("input_form"):
-            input_files = st.file_uploader(
-            "Files", type=["mp4", "m4a", "mp3", "wav"], accept_multiple_files=True)
+        st.title("Whisper")
+        if st.sidebar.form("input_form"):
+            input_files = st.file_uploader("Files", type=["mp4", "m4a", "mp3", "wav"], accept_multiple_files=True)
 
-            whisper_model = st.selectbox("Whisper model", options=[
-            "tiny", "base", "small", "medium", "large"], index=4)
+           # whisper_model = st.selectbox("Whisper model", options=[
+           # "tiny", "base", "small", "medium", "large"], index=4)
