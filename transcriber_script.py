d3143b775a9645e896bb7c37087d3bd1f70589b8
Kaktys36 <nikolaeff.alexandr2016@yandex.ru>
2024-05-04 15:27:25 +0300

    3

--- transcriber_script.py
+++ transcriber_script.py
@@ -20,6 +20,13 @@ class Transcriber:
 
         whisper_model = st.sidebar.selectbox("Whisper model", options=[
             "tiny", "base", "small", "medium", "large", "large-v2", "large-v3"], index=4)
-        device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        model = whisper.load_model(whisper_model).to('cpu')
-        st.text(device)
+        @st.cache_resource(show_spinner=False)
+    def load_whisper_model(whisper_model: str) -> whisper.Whisper:
+        try:
+            global model
+            model = whisper.load_model(whisper_model)
+            print("Model Loaded")
+            print("-------------------------")
+            return model
+        except Exception as e:
+            st.error(f"Failed to load Whisper model: {e}", icon="❌")
