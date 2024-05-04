08dd8919e5b3f1f96f197cde229c5618efa74b04
Kaktys36 <nikolaeff.alexandr2016@yandex.ru>
2024-05-04 16:09:02 +0300

    123

--- transcriber_script.py
+++ transcriber_script.py
@@ -28,7 +28,7 @@ class Transcriber:
 
         self.whisper_model = st.sidebar.selectbox("Whisper model", options=[
             "tiny", "base", "small", "medium", "large", "large-v2", "large-v3"], index=4)
-        self.model = self.load_whisper_model(whisper_model)
+        self.model = self.load_whisper_model(self.whisper_model)
     
         
    
