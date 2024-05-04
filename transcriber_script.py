6fc01919351f36a923397e8b7361fcfcc156dd0c
Kaktys36 <nikolaeff.alexandr2016@yandex.ru>
2024-05-04 13:50:59 +0300

    123

--- transcriber_script.py
+++ transcriber_script.py
@@ -1,9 +1,11 @@
 import whisper
 import streamlit as st
+import torch
 
 class Transcriber:
     def __init__(self):
-        self.test = 'asdas'#, whisper_model: str, translation: bool):
+        self.test = 'asdas',
+        self.whisper_model = "base"#, whisper_model: str, translation: bool):
         #self.translation = False
         #self.whisper_model = 'base'
         
@@ -14,7 +16,9 @@ class Transcriber:
                     Я могу писать транскрибировать аудио и видеофайлы (получать текст).
                      """
         )
-        input_files = st.sidebar.file_uploader("Files", type=["mp4", "m4a", "mp3", "wav"], accept_multiple_files=True)
+        input_file = st.sidebar.file_uploader("Files", type=["mp4", "m4a", "mp3", "wav"])
 
         whisper_model = st.sidebar.selectbox("Whisper model", options=[
-            "tiny", "base", "small", "medium", "large"], index=4)
+            "tiny", "base", "small", "medium", "large", "large-v2", "large-v3"], index=4)
+        device = 'cuda' if torch.cuda.is_available() else 'cpu'
+        model = whisper.load_model(whisper_model).to(device)
