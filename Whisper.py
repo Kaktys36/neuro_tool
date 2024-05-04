dab190b7e939641e8a45221e52ce09852a1a2e9c
Kaktys36 <nikolaeff.alexandr2016@yandex.ru>
2024-05-04 13:28:26 +0300

    123

--- Whisper.py
+++ Whisper.py
@@ -8,8 +8,8 @@ class Transcriber:
         
     def run():
         st.title("Whisper")
-        if st.sidebar.form("input_form"):
-            input_files = st.file_uploader("Files", type=["mp4", "m4a", "mp3", "wav"], accept_multiple_files=True)
+        #if st.sidebar.form("input_form"):
+         #   input_files = st.file_uploader("Files", type=["mp4", "m4a", "mp3", "wav"], accept_multiple_files=True)
 
            # whisper_model = st.selectbox("Whisper model", options=[
            # "tiny", "base", "small", "medium", "large"], index=4)
